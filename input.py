f4a8ff1f8837ccbb0a9f0d38c119be49639b7935
ErekLogic <prasang.patel2006@gmail.com>
2026-08-26 22:42:02 +0530

    Added Input Program

---
+++ input.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+print("Hello,", name)
+print("You are", age, "years old.")
+
+next_year_age = age + 1
+print("Next year, you will be", next_year_age, "years old.")
